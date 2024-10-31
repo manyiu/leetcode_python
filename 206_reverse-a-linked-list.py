377504244aeefe3cdec8e963df8a6bce4556fc0b
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-11-01 03:13:41 +0800

    fix mistake

--- 206_reverse-a-linked-list.py
+++ 206_reverse-a-linked-list.py
@@ -13,35 +13,10 @@ class Solution:
         prev = None
 
         while head:
-            # prev, prev.next, head = head, prev, head.next
+            # head, prev, prev.next = head.next, head, prev
             curr = head
             head = head.next
-            prev.next = prev
-            curr = prev
+            curr.next = prev
+            prev = curr
+
         return prev
-
-
-class TestSolution(unittest.TestCase):
-    def test_1(self):
-        head = ListNode(1)
-        head.next = ListNode(2)
-        head.next.next = ListNode(3)
-        head.next.next.next = ListNode(4)
-        head.next.next.next.next = ListNode(5)
-
-        new_head = Solution().reverseList(head)
-
-        assert new_head.val == 5
-        assert new_head.next.val == 4
-        assert new_head.next.next.val == 3
-        assert new_head.next.next.next.val == 2
-        assert new_head.next.next.next.next.val == 1
-
-    def test_2(self):
-        head = ListNode(1)
-        head.next = ListNode(2)
-
-        new_head = Solution().reverseList(head)
-
-        assert new_head.val == 2
-        assert new_head.next.val == 1
