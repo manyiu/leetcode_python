d2463ae1f50c309105883471d5b1d547acd63ab3
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-11-06 18:49:09 +0800

    110. Balanced Binary Tree add iteration solution

--- 110_balanced-binary-tree.py
+++ 110_balanced-binary-tree.py
@@ -11,18 +11,39 @@ class TreeNode:
 
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        def dfs(node: Optional[TreeNode]):
-            if not node:
-                return (True, 0)
+        stack = [root]
+        visit = [False]
+        depths = {None: 0}
 
-            left = dfs(node.left)
-            right = dfs(node.right)
+        while stack:
+            curr, visited = stack.pop(), visit.pop()
+            if curr:
+                if visited:
+                    if abs(depths[curr.left] - depths[curr.right]) > 1:
+                        return False
+                    depths[curr] = 1 + max(depths[curr.left], depths[curr.right])
+                else:
+                    stack.append(curr)
+                    visit.append(True)
+                    stack.append(curr.left)
+                    visit.append(False)
+                    stack.append(curr.right)
+                    visit.append(False)
+        return True
 
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+    # def isBalanced(self, root: Optional[TreeNode]) -> bool:
+    #     def dfs(node: Optional[TreeNode]):
+    #         if not node:
+    #             return (True, 0)
 
-            return (balanced, 1 + max(left[1], right[1]))
+    #         left = dfs(node.left)
+    #         right = dfs(node.right)
 
-        return dfs(root)[0]
+    #         balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
+
+    #         return (balanced, 1 + max(left[1], right[1]))
+
+    #     return dfs(root)[0]
 
 
 class TestSolution(unittest.TestCase):
