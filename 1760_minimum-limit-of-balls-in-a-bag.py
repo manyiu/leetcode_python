13b1056dfc8b3fceda252f8bc97d2f8fbf34fa8a
Man Yiu <11912398+manyiu@users.noreply.github.com>
2024-12-07 21:52:26 +0800

    1760. Minimum Limit of Balls in a Bag

---
+++ 1760_minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,43 @@
+import heapq
+from typing import List
+import unittest
+
+
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        left, right = 1, max(nums)
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            operationsCount = sum([(num - 1) // mid for num in nums])
+
+            if operationsCount <= maxOperations:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left
+
+
+class TestSolution(unittest.TestCase):
+    def test_0(self):
+        solution = Solution()
+        self.assertEqual(
+            solution.minimumSize([9], 2),
+            3,
+        )
+
+    def test_1(self):
+        solution = Solution()
+        self.assertEqual(
+            solution.minimumSize([2, 4, 8, 2], 4),
+            2,
+        )
+
+    def test_2(self):
+        solution = Solution()
+        self.assertEqual(
+            solution.minimumSize([7, 17], 2),
+            7,
+        )
