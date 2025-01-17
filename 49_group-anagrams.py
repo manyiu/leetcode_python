54febdf3fcc81097c71698868a0ac3e9d4c8575f
Man Yiu <11912398+manyiu@users.noreply.github.com>
2025-01-17 23:36:35 +0800

    49. Group Anagrams Add test cases

--- 49_group-anagrams.py
+++ 49_group-anagrams.py
@@ -1,23 +1,31 @@
+from collections import defaultdict
 from typing import List
 import unittest
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        group = dict()
+        groups = defaultdict(list)
 
         for str in strs:
-            sorted_str = ''.join(sorted(str))
-            if sorted_str in group:
-                group[sorted_str].append(str)
-            else:
-                group[sorted_str] = [str]
+            sorted_str = "".join(sorted(str))
+            groups[sorted_str].append(str)
 
-        return list(group.values())
+        return list(groups.values())
 
-        
 
 class TestSolution(unittest.TestCase):
-    def test(self):
+    def test_1(self):
         strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-        self.assertEqual(Solution().groupAnagrams(strs), [["eat","tea","ate"],["tan","nat"],["bat"]])
+        output = [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]]
+        self.assertEqual(Solution().groupAnagrams(strs), output)
+
+    def test_2(self):
+        strs = [""]
+        output = [[""]]
+        self.assertEqual(Solution().groupAnagrams(strs), output)
+
+    def test_3(self):
+        strs = ["a"]
+        output = [["a"]]
+        self.assertEqual(Solution().groupAnagrams(strs), output)
