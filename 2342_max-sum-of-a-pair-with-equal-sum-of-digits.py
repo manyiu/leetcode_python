d19f051321eb990b4218556655c5469357c5bed8
Man Yiu <11912398+manyiu@users.noreply.github.com>
2025-02-12 11:09:11 +0800

    2342. Max Sum of a Pair With Equal Sum of Digits
    Use two sum method

--- 2342_max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342_max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -5,19 +5,34 @@ import unittest
 
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        digitSumToValue = defaultdict(list)
-
-        for num in nums:
-            digitSumToValue[self.digitSum(num)].append(num)
-
+        hash = {}
         res = -1
 
-        for group in digitSumToValue.values():
-            if len(group) > 1:
-                a, b = sorted(group)[-2:]
-                res = max(res, a + b)
+        for num in nums:
+            currDigitSum = self.digitSum(num)
+
+            if currDigitSum in hash:
+                res = max(res, num + hash[currDigitSum])
+                hash[currDigitSum] = max(hash[currDigitSum], num)
+            else:
+                hash[currDigitSum] = num
 
         return res
+
+    # def maximumSum(self, nums: List[int]) -> int:
+    #     digitSumToValue = defaultdict(list)
+
+    #     for num in nums:
+    #         digitSumToValue[self.digitSum(num)].append(num)
+
+    #     res = -1
+
+    #     for group in digitSumToValue.values():
+    #         if len(group) > 1:
+    #             a, b = sorted(group)[-2:]
+    #             res = max(res, a + b)
+
+    #     return res
 
     # def maximumSum(self, nums: List[int]) -> int:
     #     res = -1
