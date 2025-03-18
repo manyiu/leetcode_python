bb25641e5776e5962425d9eaa28f8c053e703c3a
Man Yiu <11912398+manyiu@users.noreply.github.com>
2025-03-18 20:30:43 +0800

    206. Reverse Linked List Add test cases

--- 206_reverse-a-linked-list.py
+++ 206_reverse-a-linked-list.py
@@ -20,3 +20,25 @@ class Solution:
             prev = curr
 
         return prev
+
+
+class TestSolution(unittest.TestCase):
+    def test_1(self):
+        head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+        output = Solution().reverseList(head)
+        self.assertEqual(output.val, 5)
+        self.assertEqual(output.next.val, 4)
+        self.assertEqual(output.next.next.val, 3)
+        self.assertEqual(output.next.next.next.val, 2)
+        self.assertEqual(output.next.next.next.next.val, 1)
+
+    def test_2(self):
+        head = ListNode(1, ListNode(2))
+        output = Solution().reverseList(head)
+        self.assertEqual(output.val, 2)
+        self.assertEqual(output.next.val, 1)
+
+    def test_3(self):
+        head = None
+        output = Solution().reverseList(head)
+        self.assertEqual(output, None)
