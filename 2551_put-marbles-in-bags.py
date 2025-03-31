174c6a864a0475b9e9601761818b12e88bf1c6cc
Man Yiu <11912398+manyiu@users.noreply.github.com>
2025-03-31 10:48:04 +0800

    2551. Put Marbles in Bags

---
+++ 2551_put-marbles-in-bags.py
@@ -0,0 +1,42 @@
+from heapq import nlargest, nsmallest
+from itertools import pairwise
+from typing import List
+import unittest
+
+
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        n = len(weights) - 1
+
+        sum = [0] * n
+
+        for i in range(len(sum)):
+            sum[i] = weights[i] + weights[i + 1]
+
+        sum.sort()
+
+        res = 0
+
+        for i in range(k - 1):
+            res += sum[n - 1 - i] - sum[i]
+
+        return res
+
+    # def putMarbles(self, weights: List[int], k: int) -> int:
+    #     pair_sum = [a + b for a, b in pairwise(weights)]
+
+    #     return sum(nlargest(k - 1, pair_sum)) - sum(nsmallest(k - 1, pair_sum))
+
+
+class TestSolution(unittest.TestCase):
+    def test_example_1(self):
+        weights = [1, 3, 5, 1]
+        k = 2
+        output = 4
+        self.assertEqual(Solution().putMarbles(weights, k), output)
+
+    def test_example_2(self):
+        weights = [1, 3]
+        k = 2
+        output = 0
+        self.assertEqual(Solution().putMarbles(weights, k), output)
